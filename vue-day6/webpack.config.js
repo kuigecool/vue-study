442ac96f7274a8e14a5115f0c2b76fa0dc6689e9
xiaxia12 <1218633246@qq.com>
2019-01-31 23:15:47 +0800

    今日练习

--- vue-day6/webpack.config.js
+++
@@ -1,9 +0,0 @@
-// 导入处理路径的模块
-var path=require('path')
-module.exports = {
-    entry: './main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-      }
-  };
